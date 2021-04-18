ad9aea51901dd1ad1a6cb699a4458c7271dff6dd
Johan Viancha <vianchajohan@gmail.com>
2021-04-17 23:00:03 -0500

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompraComponent } from './components/compra/compra.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompraComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule
